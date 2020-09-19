cfbb907ae054300b64037f0dc857c7dbb2008f17
Gaurav Singh <gauravsingh@circuitvalley.com>
2020-09-20 00:21:17 +0200

    Lane aligner Timing optimizations

--- FPGA/mipi_rx_lane_aligner.v
+++ FPGA/mipi_rx_lane_aligner.v
@@ -14,6 +14,8 @@ work.  If not, see <http://creativecommons.org/licenses/by/3.0/>.
 Receives multiple lane, byte aligned data from mipi rx byte aligner @mipi byte clock  outputs lane aligned data 
 in a multi-lane mipi bus, data on different lane may appear at different offset so this module will wait till of the all lanes have valid output 
 start outputting lane aligned data so byte x from all the lanes outputted at same timescale
+
+V1.1 Sep 2020 Heavy Timing optimizations 
 */
 
 module mipi_rx_lane_aligner	(	clk_i,
@@ -25,7 +27,7 @@ module mipi_rx_lane_aligner	(	clk_i,
 
 input reset_i;
 		
-localparam [3:0]ALIGN_DEPTH = 4'h3; //how many byte misalignment is allowed, whole package length must be also longer than this 
+localparam [3:0]ALIGN_DEPTH = 4'h7; //how many byte misalignment is allowed, whole package length must be also longer than this 
 localparam [2:0]LANES = 3'h4;		//mipi csi have 2 or 4 lanes 
 
 
@@ -37,67 +39,100 @@ output reg lane_valid_o;
 
 
 reg [(8*LANES)-1:0]last_bytes[(ALIGN_DEPTH-1):0];
-reg [(LANES-1):0]last_bytes_valid;
-reg [3:0]sync_byte_index[(LANES-1):0];
-reg lane_valid_reg;
+reg [2:0]sync_byte_index[(LANES-1):0];
+reg [2:0]sync_byte_index_reg[(LANES-1):0];
+reg [2:0]offset;
 
+reg [3:0]valid;
+reg lane_valid_reg;
 reg [3:0]i;
-reg [3:0]last_sync; //which lane was delayed most , which received sync byte at last 
 
 // TODO: Find why async reset with !(|bytes_valid_i) cause whole system reset;
 
+always @(posedge clk_i)
+begin
+lane_byte_o = {last_bytes[sync_byte_index_reg[3]][31:24],
+						last_bytes[sync_byte_index_reg[2]][23:16],
+						last_bytes[sync_byte_index_reg[1]][15:8],
+						last_bytes[sync_byte_index_reg[0]][7:0]};
+end 
 always @(posedge clk_i) 
 begin
-	if (reset_i || (!lane_valid_o && (!(|bytes_valid_i))))
+	if (reset_i )
 	begin
-		lane_valid_o <= 1'h0;
-		last_bytes_valid <= 0;
-
 		for (i= 4'h0; i < ALIGN_DEPTH; i = i + 1'h1)
 		begin
 			last_bytes[i] <= 8'h0;
 		end
-		
-		for (i= 4'h0; i <LANES; i = i + 1'h1)
-		begin
-			sync_byte_index[i] = 0;
-		end
-		
-		lane_byte_o <= 0;
-		lane_valid_reg <= 1'h0;
-		last_sync <=0;
 	end
 	else
 	begin
-
-		
+				
 		last_bytes[0] <= byte_i;
 		
-		for (i= 4'h1; i < ALIGN_DEPTH; i = i + 1'h1)
+		for (i= 1'b1; i < (ALIGN_DEPTH); i = i + 1'h1)
 		begin
 			last_bytes[i] <= last_bytes[i-1'h1];
 		end
 
-		last_bytes_valid <= bytes_valid_i;
-		lane_valid_o <= last_bytes_valid[last_sync]; //one clock delay to the last packet
-		
-		if ((!lane_valid_o) && (|bytes_valid_i))
+	end
+end
+
+
+always @(posedge clk_i)
+begin
+	if (reset_i )
+	begin
+		valid <= 4'b0;
+	end
+	else
+	begin
+		valid <= bytes_valid_i;
+	end
+end
+
+always @(posedge clk_i ) 
+begin
+	if (reset_i || (!lane_valid_o && (!(|valid)))) //always reset when lane_valid_o is active, valid
+	begin		
+		for (i= 4'h0; i <LANES; i = i + 1'h1)
+		begin
+			sync_byte_index[i] <= ALIGN_DEPTH - 1'b1;
+		end
+		offset <=  ALIGN_DEPTH - 2'h2;
+	end
+	else
+	begin
+		offset <= offset - 1'b1;
+		for (i= 4'h0; i < LANES; i = i + 1'h1)			
+		begin	
+			sync_byte_index[i] <= sync_byte_index[i] - !valid[i]; //count delay of each sync, first one will be 0 delay, last one will max
+		end	
+	end
+end
+
+
+always @(posedge clk_i)
+begin
+	if (reset_i)
+	begin
+		for (i= 4'h0; i < LANES; i = i + 1'h1)			
+		begin	
+			sync_byte_index_reg[i] <= 3'h0;
+		end	
+		lane_valid_o <= 1'h0;
+	end
+	else
+	begin
+		lane_valid_reg <=( &valid)? 1'h1:(lane_valid_o && |valid)? 1'h1 : 1'h0; //one clock delay to the last packet
+		lane_valid_o <= lane_valid_reg;
+		if (!lane_valid_reg)
 		begin
 			for (i= 4'h0; i < LANES; i = i + 1'h1)			
-			begin
-				if (!bytes_valid_i[i])
-				begin
-					last_sync = i;	//which lane was last 
-					sync_byte_index[i] <= sync_byte_index[i] + 1'b1; //count delay of each sync, first one will be 0 delay last will max
-				end
-			end
+			begin	
+				sync_byte_index_reg[i] <= sync_byte_index[i] - offset;
+			end	
 		end
-			
-		
-		lane_byte_o <= {last_bytes[sync_byte_index[last_sync] - sync_byte_index[3]][31:24],
-						last_bytes[sync_byte_index[last_sync] - sync_byte_index[2]][23:16],
-						last_bytes[sync_byte_index[last_sync] - sync_byte_index[1]][15:8],
-						last_bytes[sync_byte_index[last_sync] - sync_byte_index[0]][7:0]};			
 	end
 end
 
